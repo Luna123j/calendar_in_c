63e8e2128d6eced66e9ec0741662958e4d5e877d
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-05 20:46:30 -0500

    allow enter month and year

--- calendar.c
+++ calendar.c
@@ -1,13 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main (void){
 	int month, year;
-	printf("Please enter a month, year format in mm-yyyy. \n");
-	scanf("%d-%d",month,year);
-	if (month > 12 || month <1){
-		printf("Invalid month value")
-	}
 	
-	if(year)
+	while(1){
+		printf("Please enter a month, year format in mm-yyyy. \n");
+		scanf("%d-%d",&month,&year);
+		
+		if (month > 12 || month <1){
+			printf("Invalid month value. \n");
+			continue;
+		} 
+		
+		if(year > 4902 || year < 1600){
+			printf("Invalid year value. \n");
+			continue;
+		} 
+		
+		break;
+	}
+		
+
+	
+	
 }
 
