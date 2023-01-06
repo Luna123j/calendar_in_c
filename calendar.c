2fe28a4c74366dcf3f8b894c6c6bcc76c14f4cab
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-05 20:17:23 -0500

    typo

---
+++ calendar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (void){
+	int month, year;
+	printf("Please enter a month, year format in mm-yyyy. \n");
+	scanf("%d-%d",month,year);
+	if (month > 12 || month <1){
+		printf("Invalid month value")
+	}
+	
+	if(year)
+}
+
--- calender.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main (void){
-	printf("Please enter a month, year format in mm-yyyy. \n");
-
-}
-
